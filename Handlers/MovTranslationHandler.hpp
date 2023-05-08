23af3965a3ac19f311dc9faa2164a4ed264710f0
toxicmouse7 <utiha@sasuke.ru>
2023-05-08 16:26:15 +0500

    first test pass

--- Handlers/MovTranslationHandler.hpp
+++ Handlers/MovTranslationHandler.hpp
@@ -8,26 +8,54 @@
 
 #include <Zydis/Zydis.h>
 
+#include <Requestor/Requestor.hpp>
 #include <Requestor/Interfaces/IRequestHandler.hpp>
 
 #include "Requests/MovTranslationRequest.hpp"
+#include "Requests/RegisterDownArchitectureRequest.hpp"
 
 class MovTranslationHandler : public IRequestHandler<MovTranslationRequest, std::vector<ZyanU8>>
 {
 public:
     std::vector<ZyanU8> Handle(const MovTranslationRequest& request) override
     {
+        auto& requestor = Requestor::Instance();
+        std::vector<ZyanU8> result(ZYDIS_MAX_INSTRUCTION_LENGTH);
+
         auto& instruction = request.GetInstruction();
         ZydisEncoderRequest encoderRequest;
         memset(&encoderRequest, 0, sizeof(encoderRequest));
         encoderRequest.machine_mode = ZYDIS_MACHINE_MODE_LEGACY_32;
         encoderRequest.mnemonic = ZYDIS_MNEMONIC_MOV;
+        encoderRequest.operand_count = 2;
 
-        if (instruction.operands[0].type == ZYDIS_OPERAND_TYPE_REGISTER)
+        auto& firstOperand = instruction.operands[0];
+        auto& secondOperand = instruction.operands[1];
+
+        if (firstOperand.type == ZYDIS_OPERAND_TYPE_REGISTER)
         {
+            auto downRegisterRequest = RegisterDownArchitectureRequest(instruction.operands[0].reg.value);
+            auto downRegister = requestor.Handle<ZydisRegister>(downRegisterRequest);
+            encoderRequest.operands[0].type = ZYDIS_OPERAND_TYPE_REGISTER;
+            encoderRequest.operands[0].reg.value = downRegister;
 
+            if (secondOperand.type == ZYDIS_OPERAND_TYPE_IMMEDIATE)
+            {
+                encoderRequest.operands[1].type = ZYDIS_OPERAND_TYPE_IMMEDIATE;
+                if (secondOperand.imm.value.u <= UINT32_MAX)
+                {
+                    encoderRequest.operands[1].imm.s = (int32_t)secondOperand.imm.value.s;
+                }
+            }
         }
 
-        return {};
+        ZyanUSize instructionSize = result.size();
+        auto status = ZydisEncoderEncodeInstruction(&encoderRequest, result.data(), &instructionSize);
+        if (ZYAN_FAILED(status))
+            exit(-1);
+
+        result.resize(instructionSize);
+
+        return result;
     }
 };
--- main.cpp
+++ main.cpp
@@ -1,9 +1,11 @@
+#include <fstream>
+
 #include <Requestor/Requestor.hpp>
 
 #include "Handlers/InstructionTranslationHandler.hpp"
 #include "Handlers/MovTranslationHandler.hpp"
-
-#include "Requests/InstructionTranslationRequest.hpp"
+#include "Handlers/InstructionToTextHandler.hpp"
+#include "Handlers/RegisterDownArchitectureHandler.hpp"
 
 int main()
 {
@@ -11,12 +13,37 @@ int main()
 
     try
     {
+        requestor.RegisterHandler<InstructionToTextHandler>();
         requestor.RegisterHandler<InstructionTranslationHandler>();
         requestor.RegisterHandler<MovTranslationHandler>();
+        requestor.RegisterHandler<RegisterDownArchitectureHandler>();
 
-        auto request = InstructionTranslationRequest({.info = {.mnemonic = ZYDIS_MNEMONIC_MOV}});
+        std::ifstream testFile("tests/test1.bin", std::ios::binary);
+        std::string binaryData;
+        testFile >> binaryData;
+        testFile.close();
 
-        auto result = requestor.Handle<std::vector<ZyanU8>>(request);
+        int offset = 0;
+        uintptr_t runtimeAddress = 0;
+        ZydisDisassembledInstruction instruction;
+        memset(&instruction, 0, sizeof(instruction));
+        while (ZYAN_SUCCESS(ZydisDisassembleIntel(
+                ZYDIS_MACHINE_MODE_LONG_64,
+                runtimeAddress,
+                binaryData.data() + offset,
+                binaryData.size() - offset,
+                &instruction)))
+        {
+            auto request = InstructionTranslationRequest(instruction);
+
+            auto result = requestor.Handle<std::vector<ZyanU8>>(request);
+
+            auto debugRequest = InstructionToTextRequest(result, ZYDIS_MACHINE_MODE_LEGACY_32);
+            requestor.Handle(debugRequest);
+
+            offset += instruction.info.length;
+            runtimeAddress += instruction.info.length;
+        }
     }
     catch (std::exception& e)
     {
