01c83b785e3f35a508de71ac0300c658a2a9df5c
Aleksej <utiha@sasuke.ru>
2023-05-14 18:30:09 +0500

    Added cpp tests

---
+++ tests/cpp/test3.hpp
@@ -0,0 +1,7 @@
+#pragma once
+
+class Test3
+{
+public:
+    static constexpr unsigned char data[3] = {0x48, 0x8b, 0xc3};
+};
---
+++ tests/cpp/tests.hpp
@@ -0,0 +1,6 @@
+#pragma once
+#include "test1.hpp"
+#include "test2.hpp"
+#include "test3.hpp"
+#include "test4.hpp"
+#include "test5.hpp"
